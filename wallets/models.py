fc7501a7073714df1deab2d5d2e56b10a5bcc4c0
hamed matari <hmatari@toranjcapital.com>
2024-04-26 03:51:27 +0330

    FEAT: add Transaction fields

--- wallets/models.py
+++ wallets/models.py
@@ -4,7 +4,21 @@ from django.db import models
 
 
 class Transaction(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "Pending"),
+        ("completed", "Completed"),
+        ("failed", "Failed"),
+    )
+    TYPES_CHOICES = (("withdraw", "Withdraw"), ("deposit", "Deposit"))
     amount = models.BigIntegerField()
+    wallet = models.ForeignKey(
+        "wallets.Wallet", on_delete=models.CASCADE, related_name="transactions"
+    )
+    status = models.CharField(choices=STATUS_CHOICES, default="pending", max_length=20)
+    transaction_type = models.CharField(
+        choices=TYPES_CHOICES, max_length=20, default="deposit"
+    )
+
     # todo: add fields if necessary
     pass
 
@@ -16,4 +30,3 @@ class Wallet(models.Model):
     def deposit(self, amount: int):
         # todo: deposit the amount into this wallet
         pass
-
