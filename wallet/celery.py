5d10f8c568df80e5ee796b7684adbcdfcc62ca1b
hamed matari <hmatari@toranjcapital.com>
2024-04-26 16:00:13 +0330

    FEAT: schedule beat

--- wallet/celery.py
+++ wallet/celery.py
@@ -1,8 +1,12 @@
 # celery.py
 
 from __future__ import absolute_import, unicode_literals
+
 import os
+from datetime import timedelta
+
 from celery import Celery
+from celery.schedules import crontab
 from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wallet.settings")
@@ -12,3 +16,10 @@ app = Celery("wallet")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app.conf.beat_schedule = {
+    "queue_transactions": {
+        "task": "wallet.queue_transactions",
+        "schedule": timedelta(seconds=30),
+    }
+}
